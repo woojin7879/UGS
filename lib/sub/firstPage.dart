a35a2cc6b79aaff4f34e9caacf2ec5fe8cb832c7
woojin7879 <woojin7879@unist.ac.kr>
2022-04-09 20:06:56 +0900

    로고 추가

--- lib/sub/firstPage.dart
+++ lib/sub/firstPage.dart
@@ -4,10 +4,16 @@ class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('First Page'),
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Text('UGS\nUNIST',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blue, fontSize: 40),
+            ),
+          ),
+          Image.asset('unist_logo.png', width: 1000, height: 1000),
+        ],
       ),
     );
   }
